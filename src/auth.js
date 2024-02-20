dd1809c05a3c5004441c6dece28a5cc18b6f122b
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-02-21 09:00:13 +1100

    Added changes to the function comments

--- src/auth.js
+++ src/auth.js
@@ -1,15 +1,17 @@
 /**
-  * Register a user with an email, password, and names, then returns their authUserId value
+  * Register a user with an email, password, and first and last name.
+  * Returns the authUserId of the user.
   * 
   * @param {string} email - the email of the user
   * @param {string} password - the password of the user
   * @param {string} nameFirst - the first name of the user
   * @param {string} nameLast - the last name of the user
   * 
-  * @returns {{authUserId: 1}} - object 
+  * @returns {{authUserId: number}} - object containing the authUserId of the user
 */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
+    // TODO: Implement this function
     return {
         authUserId: 1,
-    }    
+    };    
 }
