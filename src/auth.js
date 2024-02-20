fbf6fd4df17e27bf1521bda7809d4199e394ce35
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-02-21 08:34:19 +1100

    Implemented the stub function for adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,15 @@
+/**
+  * Register a user with an email, password, and names, then returns their authUserId value
+  * 
+  * @param {string} email - the email of the user
+  * @param {string} password - the password of the user
+  * @param {string} nameFirst - the first name of the user
+  * @param {string} nameLast - the last name of the user
+  * 
+  * @returns {{authUserId: 1}} object 
+*/
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    }    
+}
