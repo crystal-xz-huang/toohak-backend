4766a0b9662af325f881e483704ae2d26026635d
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-02-21 08:56:00 +1100

    Implemented the stub function for adminAuthLogin

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+/**
+  * Given a registered user's email and password returns their authUserId value
+  * 
+  * @param {string} email - email of a registered user
+  * @param {string} password - password of a registered user
+  * 
+  * @returns {{authUserId: number}} - object containing the authUserId of the user 
+*/
+function adminAuthLogin(email, password) {
+    // TODO: Implement this function 
+    return {
+        authUserId: 1,
+    };
+}
