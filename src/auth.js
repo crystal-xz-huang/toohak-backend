e5272b6b15eaadc928f2810edf677d783554dfd9
Keirvi Vadera <z5449687@ad.unsw.edu.au>
2024-02-21 13:29:29 +1100

    Finished stubbing for adminUserPasswordUpdate

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,13 @@
-// adminUserPasswordUpdate : Given details relating to a password change,
-// update the password of a logged in user.
+/**
+  * Given details relating to a password change, update the 
+  * password of a logged in user.
+  * 
+  * @param {integer} authUserId - the id of registered user
+  * @param {string} oldPassword - the old password of registered user
+  * @param {string} newPassword - the new password of registered user
+  * 
+  * @returns { } - returns nothing
+*/
 
 function adminUserPasswordUpdate ( authUserId, oldPassword, newPassword ) {
     return {};
