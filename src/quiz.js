7e0bd17fb2331d9a77ab06dfb6920879ab677cdd
Andy Huang <z5479149@ad.unsw.edu.au>
2024-02-22 07:18:35 +1100

    Added documentation to function adminQuizList

--- src/quiz.js
+++ src/quiz.js
@@ -15,6 +15,15 @@ function adminQuizCreate ( authUserId, name, description ) {
     }
 }
 
+/**
+  *Given an id of registered user,
+  * return list of quizzes owned by user.
+  * 
+  * @param {integer} authUserId - the id of registered user
+  * @returns {quizzes: [ { quizId: number, name: name } ] } - object containing
+  * quizzes of the user
+*/
+
 function adminQuizList ( authUserId ) {
   return {
     quizzes: [
@@ -26,9 +35,31 @@ function adminQuizList ( authUserId ) {
   };
 }
 
+/**
+  *Given basic details about a new quiz,
+  * create one for the logged in user.
+  * 
+  * @param {integer} authUserId - the id of registered user
+  * @param {string} name - the name of registered user
+  * @param {string} description - basic details about the quiz
+  * 
+  * @returns { quizId: number } - object containing quizId of the user
+*/
+
 function adminQuizRemove(authUserId, quizId) {
   return {};
 }
+
+/**
+  *Given basic details about a new quiz,
+  * create one for the logged in user.
+  * 
+  * @param {integer} authUserId - the id of registered user
+  * @param {string} name - the name of registered user
+  * @param {string} description - basic details about the quiz
+  * 
+  * @returns { quizId: number } - object containing quizId of the user
+*/
 
 function adminQuizInfo(authUserId, quizId) {
   return {
