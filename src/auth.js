ff3390dc73c64761f717ef47c6df63666fe827bf
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-02-22 02:53:30 +1100

    Resolved conflicts

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,20 @@
 /**
+* Given a registered user's email and password, returns their authUserId value
+* 
+* @param {string} email - the email of a registered user
+* @param {string} password - the password of a registered user
+* 
+* @returns {{authUserId: number}} - object containing the authUserId of the user 
+*/
+function adminAuthLogin(email, password) {
+  // TODO: Implement this function 
+  return {
+      authUserId: 1,
+  };
+}
+
+
+/**
   * Register a user with an email, password, and first and last name.
   * Returns the authUserId of the user.
   * 
@@ -10,8 +26,8 @@
   * @returns {{authUserId: number}} - object containing the authUserId of the user
 */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
-    // TODO: Implement this function
-    return {
-        authUserId: 1,
-    };    
+  // TODO: Implement this function
+  return {
+      authUserId: 1,
+  };    
 }
