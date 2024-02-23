3d0b35b78a1399f4d5ac7cbe1f4db634a9440db9
Cale <z5416920@ad.unsw.edu.au>
2024-02-23 14:35:59 +1100

    Implemented the stubbing for clear function

---
+++ src/other.js
@@ -0,0 +1,10 @@
+/**
+  * Reset the state of the application back to the start.
+  * 
+  * @param { } - has no parameters
+  * 
+  * @returns { } - returns nothing
+*/
+function clear() {
+    return {};
+}
