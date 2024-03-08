301627b4f1aad2235f0be4e6a1c11c94b6b3348e
Andy Huang <z5479149@ad.unsw.edu.au>
2024-03-08 21:38:06 +1100

    Replaced literals with defined constants in unit testing for function adminQuizInfo in quiz.test.js

--- src/tests/quiz.test.js
+++ src/tests/quiz.test.js
@@ -311,8 +311,8 @@ describe('testing adminQuizInfo', () => {
     let quizId;
 
     beforeEach(() => {
-        userId = adminAuthRegister('janedoe@gmail.com', 'hashed_password1', 'Jane', 'Doe');
-        quizId = adminQuizCreate(userId.authUserId, 'Quiz 1', 'This is a quiz');
+        userId = adminAuthRegister(USER1.email, USER1.password, USER1.nameFirst, USER1.nameLast);
+        quizId = adminQuizCreate(userId.authUserId, QUIZNAME1, QUIZDESCRIPTION1);
     })
 
     test('returns error with an invalid userId', () => {
@@ -324,31 +324,31 @@ describe('testing adminQuizInfo', () => {
     })
 
     test('returns error for requesting info about a quiz not owned by the user', () => {
-        let userId2 = adminAuthRegister('johnsmith@gmail.com', 'hashed_password2', 'John', 'Smith');
+        let userId2 = adminAuthRegister(USER2.email, USER2.password, USER2.nameFirst, USER2.nameLast);
         expect(adminQuizInfo(userId2.authUserId, quizId.quizId)).toStrictEqual(ERROR);
     })
 
     test('returning info of one quiz created by one user', () => {
         let returnObject = {
             quizId: quizId.quizId,
-            name: 'Quiz 1',
+            name: QUIZNAME1,
             timeCreated: expect.any(Number),
             timeLastEdited: expect.any(Number),
-            description: 'This is a quiz',
+            description: QUIZDESCRIPTION1,
         }
         expect(adminQuizInfo(userId.authUserId, quizId.quizId)).toStrictEqual(returnObject);
     })
 
     test('returning info of second quiz created by second user', () => {
-        let userId2 = adminAuthRegister('johnsmith@gmail.com', 'hashed_password2', 'John', 'Smith');
-        let quizId2 = adminQuizCreate(userId2.authUserId, 'Quiz 2', 'This is a quiz2');
+        let userId2 = adminAuthRegister(USER2.email, USER2.password, USER2.nameFirst, USER2.nameLast);
+        let quizId2 = adminQuizCreate(userId2.authUserId, QUIZNAME2, QUIZDESCRIPTION2);
 
         let returnObject = {
             quizId: quizId2.quizId,
-            name: 'Quiz 2',
+            name: QUIZNAME2,
             timeCreated: expect.any(Number),
             timeLastEdited: expect.any(Number),
-            description: 'This is a quiz2',
+            description: QUIZDESCRIPTION2,
         }
 
         expect(adminQuizInfo(userId2.authUserId, quizId2.quizId)).toStrictEqual(returnObject);
