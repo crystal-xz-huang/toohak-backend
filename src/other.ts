8c43691a4bd4eb65386f0191331ae66feb9f7790
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-03-16 05:07:57 +1100

    Completed DELETE /v1/clear route and function implementation in server.ts and other.ts

--- src/other.ts
+++ src/other.ts
@@ -4,15 +4,17 @@ import { EmptyObject } from './types';
 /**
   * Reset the state of the application back to the start
   *
-  * @param {} - no parameters
-  * @returns {EmptyObject} - returns nothing
+  * @param { void } 
+  * @returns { EmptyObject } 
 */
 export function clear(): EmptyObject {
   const data = getData();
   data.users = [];
   data.quizzes = [];
+  data.sessions = [];
   data.userId_counter = 0;
   data.quizId_counter = 0;
+  data.sessionId_counter = 0;
   setData(data);
   return {};
 }
