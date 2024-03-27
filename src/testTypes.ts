1b3078f7c4afdceb87de7d9bc7c40a9e349ccf2a
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-03-28 08:16:42 +1100

    Completed initial tests for POST /v1/admin/quiz/{quizid}/question in quiz.test.ts

--- src/testTypes.ts
+++ src/testTypes.ts
@@ -66,17 +66,274 @@ export const quiz3 = {
   description: 'This is yet another quiz',
 };
 
+export const quiz4 = {
+  name: 'Quiz 4',
+  description: 'This is once again another quiz',
+};
+
 export const shortQuizNames = [
   { name: '' },
   { name: 'a' },
   { name: 'ab' },
 ];
 
-export const badRequestErrorQuizNames = [
+export const invalidQuizNames = [
   { name: '' },
   { name: 'a' },
+  { name: 'ab' },
   { name: 'Quiz 1&!' },
   { name: 'a'.repeat(31) },
 ];
 
 export const invalidQuizDescription = 'a'.repeat(101); // more than 100 characters
+
+export const validQuestion1 = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+  ]
+};
+
+export const validQuestion2 = {
+  question: 'What is the capital of Australia?',
+  duration: 3,
+  points: 5,
+  answers: [
+    { answer: 'Canberra', correct: true },
+    { answer: 'Sydney', correct: false },
+  ]
+};
+
+// Question string is less than 5 characters in length
+export const shortQuestionString = [
+  {
+    question: '',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Queen Elizabeth II', correct: false },
+    ]
+  },
+  {
+    question: 'a'.repeat(4),
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Queen Elizabeth II', correct: false },
+      { answer: 'Prince William', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+];
+
+// Question string is greater than 50 characters in length
+export const longQuestionString = {
+  question: 'a'.repeat(51),
+  duration: 4,
+  points: 5,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+  ]
+};
+
+// Question has more than 6 answers
+export const moreQuestionAnswers = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+    { answer: 'Prince Andrew', correct: false },
+    { answer: 'Princess Anne', correct: false },
+  ]
+};
+
+// Question has less than 2 answers
+export const lessQuestionAnswers = [
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [],
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+    ]
+  },
+];
+
+// Question duration is not a positive number
+export const negativeQuestionDuration = [
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 0,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Queen Elizabeth II', correct: false },
+      { answer: 'Prince William', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: -1,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Queen Elizabeth II', correct: false },
+      { answer: 'Prince William', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+];
+
+// Sum of the question durations in the quiz exceeds 3 minutes
+export const moreQuestionDurationSum = {
+  question: 'What is the capital of Australia?',
+  duration: 181,
+  points: 5,
+  answers: [
+    { answer: 'Canberra', correct: true },
+    { answer: 'Sydney', correct: false },
+    { answer: 'Melbourne', correct: false },
+    { answer: 'Brisbane', correct: false },
+  ]
+};
+
+// Points awarded for the question is less than 1
+export const lessQuestionPoints = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 0,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+  ]
+};
+
+// Points awarded for the question is greater than 10
+export const moreQuestionPoints = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 11,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+  ]
+};
+
+// Length of any answer is shorter than 1 character long
+export const shortQuestionAnswers = [
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: '', correct: true },
+      { answer: 'Prince William', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: '', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Prince William', correct: false },
+      { answer: '', correct: false },
+    ]
+  },
+];
+
+// Length of any answer is longer than 30 characters long
+export const longQuestionAnswers = [
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'a'.repeat(31), correct: true },
+      { answer: 'Prince William', correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'a'.repeat(31), correct: false },
+      { answer: 'Prince Harry', correct: false },
+    ]
+  },
+  {
+    question: 'Who is the Monarch of England?',
+    duration: 4,
+    points: 5,
+    answers: [
+      { answer: 'Prince Charles', correct: true },
+      { answer: 'Prince William', correct: false },
+      { answer: 'a'.repeat(31), correct: false },
+    ]
+  },
+];
+
+// Any answer strings are duplicates of one another (within the same question)
+export const duplicateQuestionAnswers = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Prince Charles', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince William', correct: false },
+  ]
+};
+
+// There are no correct answers
+export const falseQuestionAnswers = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    { answer: 'Prince Charles', correct: false },
+    { answer: 'Queen Elizabeth II', correct: false },
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Harry', correct: false },
+  ]
+};
