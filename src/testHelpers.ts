ae0fb8f8391db729aac3f850c89e34926a92fc74
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-04-02 06:54:44 +1100

    requestHelper function now takes headers as an optional parameter in testHelpers.ts

--- src/testHelpers.ts
+++ src/testHelpers.ts
@@ -17,6 +17,7 @@ interface RequestResponse {
  * @param { string } method     - the type of HTTP request (GET, DELETE, POST, PUT)
  * @param { string } path       - the route of the request on the server
  * @param { object } payload    - the data to send with the request (either a query string or a request body)
+ * @param { object } headers    - the headers to send with the request (only used for requests that require authentication)
  * @returns { RequestResponse } - the response from the server which is an object defined by RequestResponse
  *
  * Errors will be returned in the form:
@@ -25,11 +26,7 @@ interface RequestResponse {
  * Normal responses will be in the form
  *  { statusCode: number, jsonBody: object }
  */
-function requestHelper(
-  method: HttpVerb,
-  path: string,
-  payload: object = {}
-): RequestResponse {
+function requestHelper(method: HttpVerb, path: string, payload: object, headers?: { token: string }): RequestResponse {
   let qs = {};
   let json = {};
 
@@ -41,35 +38,25 @@ function requestHelper(
     json = payload;
   }
 
-  // Send the request to the server
-  // const res = request(method, SERVER_URL + path, { qs, json });
-
-  const res = request(method, SERVER_URL + path, { qs, json, timeout: 20000 });
-
+  // Send the request to the server with a timeout of 100ms
+  const res = request(method, SERVER_URL + path, { qs, json, timeout: 100, headers: headers });
   const bodyString = res.body as string;
-  let bodyObject: RequestResponse;
 
   try {
     // Try to parse the server's response as JSON
-    if (res.statusCode !== 200) {
-      bodyObject = {
-        statusCode: res.statusCode,
-        error: JSON.parse(bodyString).error,
-      };
+    const parsedBody = JSON.parse(bodyString);
+    if (parsedBody.error) {
+      return { statusCode: res.statusCode, error: parsedBody.error };
     } else {
-      bodyObject = {
-        statusCode: res.statusCode,
-        jsonBody: JSON.parse(bodyString),
-      };
+      return { statusCode: res.statusCode, jsonBody: parsedBody };
     }
   } catch (error) {
     // If JSON.parse fails, the server's response is not JSON so we return the response as a custom error message instead
-    bodyObject = {
+    return {
       statusCode: res.statusCode,
       error: `Server returned an invalid JSON response with error: ${error.message}`,
     };
   }
-  return bodyObject;
 }
 
 // ========================================================================= //
@@ -83,39 +70,39 @@ export function authLoginV1(email: string, password: string): RequestResponse {
 }
 
 export function userDetailsV1(token: string): RequestResponse {
-  return requestHelper('GET', '/v1/admin/user/details', { token });
+  return requestHelper('GET', '/v1/admin/user/details', {}, { token });
 }
 
 export function userDetailsUpdateV1(token: string, email: string, nameFirst: string, nameLast: string): RequestResponse {
-  return requestHelper('PUT', '/v1/admin/user/details', { token, email, nameFirst, nameLast });
+  return requestHelper('PUT', '/v1/admin/user/details', { email, nameFirst, nameLast }, { token });
 }
 
 export function userPasswordUpdateV1(token: string, oldPassword: string, newPassword: string): RequestResponse {
-  return requestHelper('PUT', '/v1/admin/user/password', { token, oldPassword, newPassword });
+  return requestHelper('PUT', '/v1/admin/user/password', { oldPassword, newPassword }, { token });
 }
 
 export function quizListV1(token: string): RequestResponse {
-  return requestHelper('GET', '/v1/admin/quiz/list', { token });
+  return requestHelper('GET', '/v1/admin/quiz/list', {}, { token });
 }
 
 export function quizCreateV1(token: string, name: string, description: string): RequestResponse {
-  return requestHelper('POST', '/v1/admin/quiz', { token, name, description });
+  return requestHelper('POST', '/v1/admin/quiz', { name, description }, { token });
 }
 
 export function quizRemoveV1(token: string, quizId: number): RequestResponse {
-  return requestHelper('DELETE', `/v1/admin/quiz/${quizId}`, { token });
+  return requestHelper('DELETE', `/v1/admin/quiz/${quizId}`, {}, { token });
 }
 
 export function quizInfoV1(token: string, quizId: number): RequestResponse {
-  return requestHelper('GET', `/v1/admin/quiz/${quizId}`, { token });
+  return requestHelper('GET', `/v1/admin/quiz/${quizId}`, {}, { token });
 }
 
 export function quizNameUpdateV1(token: string, quizId: number, name: string): RequestResponse {
-  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/name`, { token, name });
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/name`, { name }, { token });
 }
 
 export function quizDescriptionUpdateV1(token: string, quizId: number, description: string): RequestResponse {
-  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/description`, { token, description });
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/description`, { description }, { token });
 }
 
 export function clearV1(): RequestResponse {
@@ -123,41 +110,104 @@ export function clearV1(): RequestResponse {
 }
 
 export function authLogoutV1(token: string): RequestResponse {
-  return requestHelper('POST', '/v1/admin/auth/logout', { token });
+  return requestHelper('POST', '/v1/admin/auth/logout', {}, { token });
 }
 
 export function quizTrashViewV1(token: string): RequestResponse {
-  return requestHelper('GET', '/v1/admin/quiz/trash', { token });
+  return requestHelper('GET', '/v1/admin/quiz/trash', {}, { token });
 }
 
 export function quizRestoreV1(token: string, quizId: number): RequestResponse {
-  return requestHelper('POST', `/v1/admin/quiz/${quizId}/restore`, { token });
+  return requestHelper('POST', `/v1/admin/quiz/${quizId}/restore`, {}, { token });
 }
 
 export function quizTrashEmptyV1(token: string, quizIds: number[]): RequestResponse {
-  return requestHelper('DELETE', '/v1/admin/quiz/trash/empty', { token, quizIds: JSON.stringify(quizIds) });
+  return requestHelper('DELETE', '/v1/admin/quiz/trash/empty', { quizIds: JSON.stringify(quizIds) }, { token });
 }
 
 export function quizTransferV1(token: string, quizId: number, userEmail: string): RequestResponse {
-  return requestHelper('POST', `/v1/admin/quiz/${quizId}/transfer`, { token, userEmail });
+  return requestHelper('POST', `/v1/admin/quiz/${quizId}/transfer`, { userEmail }, { token });
 }
 
 export function quizQuestionCreateV1(token: string, quizId: number, questionBody: object): RequestResponse {
-  return requestHelper('POST', `/v1/admin/quiz/${quizId}/question`, { token, questionBody });
+  return requestHelper('POST', `/v1/admin/quiz/${quizId}/question`, { questionBody }, { token });
 }
 
 export function quizQuestionUpdateV1(token: string, quizId: number, questionId: number, questionBody: object): RequestResponse {
-  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/question/${questionId}`, { token, questionBody });
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/question/${questionId}`, { questionBody }, { token });
 }
 
 export function quizQuestionRemoveV1(token: string, quizId: number, questionId: number): RequestResponse {
-  return requestHelper('DELETE', `/v1/admin/quiz/${quizId}/question/${questionId}`, { token });
+  return requestHelper('DELETE', `/v1/admin/quiz/${quizId}/question/${questionId}`, {}, { token });
 }
 
 export function quizQuestionMoveV1(token: string, quizId: number, questionId: number, newPosition: number): RequestResponse {
-  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/question/${questionId}/move`, { token, newPosition });
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/question/${questionId}/move`, { newPosition }, { token });
 }
 
 export function quizQuestionDuplicateV1(token: string, quizId: number, questionId: number): RequestResponse {
-  return requestHelper('POST', `/v1/admin/quiz/${quizId}/question/${questionId}/duplicate`, { token });
+  return requestHelper('POST', `/v1/admin/quiz/${quizId}/question/${questionId}/duplicate`, {}, { token });
+}
+
+/***********************************************************************
+* Iteration 3
+***********************************************************************/
+export function quizThumbnailUpdateV1(token: string, quizId: number, imgUrl: string): RequestResponse {
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/thumbnail`, { imgUrl }, { token });
+}
+
+export function quizSessionListV1(token: string, quizId: number): RequestResponse {
+  return requestHelper('GET', `/v1/admin/quiz/${quizId}/sessions`, {}, { token });
+}
+
+export function quizSessionStartV1(token: string, quizId: number, autoStartNum: number): RequestResponse {
+  return requestHelper('POST', `/v1/admin/quiz/${quizId}/session/start`, { autoStartNum }, { token });
+}
+
+export function adminQuizSessionUpdateV1(token: string, quizId: number, sessionId: number, action: string): RequestResponse {
+  return requestHelper('PUT', `/v1/admin/quiz/${quizId}/session/${sessionId}`, { action }, { token });
+}
+
+export function quizSessionStatusV1(token: string, quizId: number, sessionId: number): RequestResponse {
+  return requestHelper('GET', `/v1/admin/quiz/${quizId}/session/${sessionId}`, {}, { token });
+}
+
+export function quizSessionResultsV1(token: string, quizId: number, sessionId: number): RequestResponse {
+  return requestHelper('GET', `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, {}, { token });
+}
+
+export function quizSessionCSVResultsV1(token: string, quizId: number, sessionId: number): RequestResponse {
+  return requestHelper('GET', `/v1/admin/quiz/${quizId}/session/${sessionId}/results/csv`, {}, { token });
+}
+
+export function playerJoinV1(sessionId: number, name: string): RequestResponse {
+  return requestHelper('POST', '/v1/player/join', { sessionId, name });
+}
+
+export function playerStatusV1(playerId: number): RequestResponse {
+  return requestHelper('GET', `/v1/player/${playerId}`, {});
+}
+
+export function playerQuestionInfoV1(playerId: number, questionPosition: number): RequestResponse {
+  return requestHelper('GET', `/v1/player/${playerId}/question/${questionPosition}`, {});
+}
+
+export function playerQuestionAnswerV1(playerId: number, questionPosition: number, answerIds: number[]): RequestResponse {
+  return requestHelper('PUT', `/v1/player/${playerId}/question/${questionPosition}/answer`, { answerIds });
+}
+
+export function playerQuestionResultsV1(playerId: number, questionPosition: number): RequestResponse {
+  return requestHelper('GET', `/v1/player/${playerId}/question/${questionPosition}/results`, {});
+}
+
+export function playerFinalResultsV1(playerId: number): RequestResponse {
+  return requestHelper('GET', `/v1/player/${playerId}/results`, {});
+}
+
+export function playerChatListV1(playerId: number): RequestResponse {
+  return requestHelper('GET', `/v1/player/${playerId}/chat`, {});
+}
+
+export function playerChatSendV1(playerId: number, message: string): RequestResponse {
+  return requestHelper('POST', `/v1/player/${playerId}/chat`, { message });
 }
