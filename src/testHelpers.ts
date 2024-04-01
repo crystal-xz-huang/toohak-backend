f6f571a26b614efc679876733f2fe9cb28999792
Crystal Huang <z5215571@ad.unsw.edu.au>
2024-04-02 07:15:38 +1100

    Fixed linting errors in testHelpers.ts

--- src/testHelpers.ts
+++ src/testHelpers.ts
@@ -38,7 +38,7 @@ function requestHelper(method: HttpVerb, path: string, payload: object, headers?
     json = payload;
   }
 
-  // Send the request to the server with a timeout 
+  // Send the request to the server with a timeout
   const res = request(method, SERVER_URL + path, { qs, json, timeout: 2000, headers: headers });
   const bodyString = res.body as string;
 
